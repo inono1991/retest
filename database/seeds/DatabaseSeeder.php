0346633320c4f0f714586d5b4de7713fe06c9c42
inono1991 <2998604835@qq.com>
2018-10-28 14:05:57 +0800

    seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,35 +13,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        DB::table('roles')->insert([
-//            ['name' => 'administrators', 'level' => 1, 'describe' => '超级管理员', 'status' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-//            ['name' => 'groups', 'level' => 2, 'describe' => '组用户', 'status' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-//            ['name' => 'users', 'level' => 3, 'describe' => '普通用户', 'status' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-//            ['name' => 'guests', 'level' => 4, 'describe' => '游客', 'status' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
-//        ]);
-//        DB::table('permissions')->insert([
-//            ['name' => 'insert', 'describe' => '写', 'status' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-//            ['name' => 'read', 'describe' => '读', 'status' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-//            ['name' => 'update', 'describe' => '修改', 'status' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-//            ['name' => 'delete', 'describe' => '删除', 'status' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
-//        ]);
+        DB::table('roles')->insert([
+            ['name' => 'administrators', 'level' => 1, 'describe' => '超级管理员', 'status' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'groups', 'level' => 2, 'describe' => '组用户', 'status' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'users', 'level' => 3, 'describe' => '普通用户', 'status' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'guests', 'level' => 4, 'describe' => '游客', 'status' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+        ]);
+        DB::table('permissions')->insert([
+            ['name' => 'insert', 'describe' => '写', 'status' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'read', 'describe' => '读', 'status' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'update', 'describe' => '修改', 'status' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'delete', 'describe' => '删除', 'status' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+        ]);
 
         $roles = DB::table('roles')->where('status', 1)->pluck('id', 'name')->toArray();
         $permissions = DB::table('permissions')->where('status', 1)->pluck('id', 'name')->toArray();
-//        DB::table('role_permission')->insert([
-//            ['role_id' => $roles['administrators'], 'permission_id' => $permissions['insert']],
-//            ['role_id' => $roles['administrators'], 'permission_id' => $permissions['read']],
-//            ['role_id' => $roles['administrators'], 'permission_id' => $permissions['update']],
-//            ['role_id' => $roles['administrators'], 'permission_id' => $permissions['delete']],
-//            ['role_id' => $roles['groups'], 'permission_id' => $permissions['insert']],
-//            ['role_id' => $roles['groups'], 'permission_id' => $permissions['read']],
-//            ['role_id' => $roles['groups'], 'permission_id' => $permissions['update']],
-//            ['role_id' => $roles['users'], 'permission_id' => $permissions['insert']],
-//            ['role_id' => $roles['users'], 'permission_id' => $permissions['read']],
-//            ['role_id' => $roles['guests'], 'permission_id' => $permissions['read']],
-//        ]);
+        DB::table('role_permission')->insert([
+            ['role_id' => $roles['administrators'], 'permission_id' => $permissions['insert']],
+            ['role_id' => $roles['administrators'], 'permission_id' => $permissions['read']],
+            ['role_id' => $roles['administrators'], 'permission_id' => $permissions['update']],
+            ['role_id' => $roles['administrators'], 'permission_id' => $permissions['delete']],
+            ['role_id' => $roles['groups'], 'permission_id' => $permissions['insert']],
+            ['role_id' => $roles['groups'], 'permission_id' => $permissions['read']],
+            ['role_id' => $roles['groups'], 'permission_id' => $permissions['update']],
+            ['role_id' => $roles['users'], 'permission_id' => $permissions['insert']],
+            ['role_id' => $roles['users'], 'permission_id' => $permissions['read']],
+            ['role_id' => $roles['guests'], 'permission_id' => $permissions['read']],
+        ]);
         $roleIds = array_values($roles);
-        for ($i = 0; $i < 30; $i++) {
+        for ($i = 0; $i < 300; $i++) {
             $user = new \App\User();
             $name = str_random(10);
             $user->name = $name;
